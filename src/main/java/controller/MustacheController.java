4906f3b770cb78a73ab2a5db3f6d3329f50cfe49
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-07 11:42:09 +0900

    @Controller 선언

--- src/main/java/controller/MustacheController.java
+++ src/main/java/controller/MustacheController.java
@@ -1,8 +1,9 @@
 package controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Controller
 public class MustacheController {
     @GetMapping(value = "/hi")
     public String mustacheCon(Model model){
